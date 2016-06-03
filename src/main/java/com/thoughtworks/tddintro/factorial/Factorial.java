30e64ca856d2681ce852262459d223caf7cf8aa5
Slohmes <sarahlohmes@gmail.com>
2016-06-03 17:48:45 -0400

    Made failing Factorial tests pass.

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,12 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i < 0) {
+            throw new IllegalArgumentException("Received negative number as input.");
+        }
+        if (i == 0) {
+            return 1;
+        }
+        return i * compute(i-1);
     }
 }
