60642efe21d3e3c0e178d4e90f833755aad0736f
Slohmes <sarahlohmes@gmail.com>
2016-06-04 00:36:54 -0400

    Made second AccountBalance test pass, refactored.

--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,20 +1,27 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTests {
+    private AccountBalance balance = new AccountBalance(100);
+
+    @Before
+    public void setUp() throws Exception {
+        balance = new AccountBalance(100);
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        AccountBalance balance = new AccountBalance(100);
         assertThat(balance.deposit(50), is(150));
     }
-//
-//    @Test
-//    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-//
-//    }
+
+    @Test
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        assertThat(balance.withdraw(50), is(50));
+    }
 //
 //    @Test
 //    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
